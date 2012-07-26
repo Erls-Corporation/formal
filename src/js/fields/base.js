a084416036b33a37e7a9521f38b32fc79c67cdc6
Chiel Kunkels <chiel@kunkels.me>
2012-07-26 16:19:53 +0100

    Change the way dependancies are built/used
    Instead of deleting the group entirely every time now, it'll simply only
    remove the fieldset from the DOM, causing the fields not to be
    submitted. However, doing it this way means user input is saved, in case
    the dependancy is triggered again (think accidental clicks on radio
    buttons or whatever).
    
    Also made sure the received value is always an array, in case of
    checkboxes/multiple selects.

--- src/js/fields/base.js
+++ src/js/fields/base.js
@@ -3,26 +3,38 @@
 var groupTypes; // gets required in initialise due to cyclic dependancy
 
 module.exports = new Class({
+	activeGroups: [],
+	builtGroups: {},
+
 	/**
 	 * Check if any dependancies should be triggered
 	 */
 	checkDependancies: function(){
 		groupTypes = groupTypes || require('./../grouptypes');
 
-		var group;
-		while (this.activeGroups.length) {
-			group = this.activeGroups.pop();
-			group.destroy();
-			delete group;
+		var self = this, values = this.getValue(), key, value;
+		if (typeOf(values) !== 'array') {
+			values = [values];
 		}
 
-		var self = this, value = this.getValue(), groupObj;
-		if (value in this.spec.dependancies) {
-			Array.each(this.spec.dependancies[value], function(group){
-				groupObj = new groupTypes[group.type](self.li, group)
-				groupObj.attach();
-				self.activeGroups.push(groupObj);
-			});
+		while (this.activeGroups.length) {
+			this.activeGroups.pop().detach();
+		}
+
+		while(values.length) {
+			value = values.shift();
+			if (value in this.spec.dependancies) {
+				if (!(value in this.builtGroups)) {
+					this.builtGroups[value] = [];
+					Array.each(this.spec.dependancies[value], function(group){
+						self.builtGroups[value].push(new groupTypes[group.type](self.li, group));
+					});
+				}
+				Array.each(this.builtGroups[value], function(group){
+					self.activeGroups.push(group);
+					group.attach();
+				});
+			}
 		}
 	},
 
