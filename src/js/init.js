6b1ce622fcbfe107a5e51b9c425e9644680af1ea
Chiel Kunkels <chiel@kunkels.me>
2012-08-13 11:17:49 +0100

    Added a public api to register new types
    Now you can do `Formal.registerPage`, `Formal.registerGroup` and
    `Formal.registerField` to register your own types. Note that these are
    not tied to a specific instance of Formal, but are instead global to all
    instances on the page.

--- src/js/init.js
+++ src/js/init.js
@@ -3,7 +3,8 @@
 
 var pages = require('./pages'),
 	groups = require('./groups'),
-	fields = require('./fields');
+	fields = require('./fields'),
+	Core = require('./core');
 
 pages.register('main', require('./pages/main'));
 
@@ -14,5 +15,52 @@ fields.register('select', require('./fields/select'));
 fields.register('radio', require('./fields/radio'));
 fields.register('checkbox', require('./fields/checkbox'));
 
-window.Formal = require('./core');
+/**
+ * Public api
+ */
+window.Formal = {
+	/**
+	 * Create a new Formal instance
+	 * @param {Element} root Element to build the form in
+	 * @param {Array} spec Specification of the form
+	 * @return {Object} A new Formal instance
+	 */
+	init: function(root, spec){
+		return new Core(root, spec);
+	},
+
+	/**
+	 * Base types which can be used to extend custom types
+	 */
+	Bases: {
+		Field: require('./fields/base')
+	},
+
+	/**
+	 * Register a new page
+	 * @param {String} type Type of page
+	 * @param {Class} definition Definition of the page
+	 */
+	registerPage: function(type, definition){
+		pages.register(type, definition);
+	},
+
+	/**
+	 * Register a new group
+	 * @param {String} type Type of group
+	 * @param {Class} definition Definition of the group
+	 */
+	registerGroup: function(type, definition){
+		groups.register(type, definition);
+	},
+
+	/**
+	 * Register a new field
+	 * @param {String} type Type of field
+	 * @param {Class} definition Definition of the field
+	 */
+	registerField: function(type, definition){
+		fields.register(type, definition);
+	}
+};
 
